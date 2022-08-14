68f44dcc02789e3e86f750e45bde071de9d9ce46
yk0613 <you@example.com>
2022-08-14 16:18:11 +0900

    unsplashAPIの設定

---
+++ pages/api/unsplash.ts
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+const endpoint = "https://api.unsplash.com/";
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ name: "John Doe" });
+}
+
+// const nextFunction = () => {
+//   fetch(
+//     `https://api.unsplash.com/search/photos?query=${router.query.q}&client_id=${process.env.NEXT_PUBLIC_APIKEY}&per_page=24&order_by=popular&page=${page}`
+//   )
+//     .then((data) => data.json())
+//     .then((imgData: ISearchResponse) => {
+//       images?.results?.push(...imgData.results);
+//       setPage(page + 1);
+//     })
+//     .catch((err) => console.log(err));
+// };
