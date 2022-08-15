eaecd3e337cd02ee317288cd3c80b24a2992ca85
yk0613 <you@example.com>
2022-08-15 19:30:46 +0900

    next.configの変更

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
-const WindiCSSWebpackPlugin = require("windicss-webpack-plugin");
-
-/** @type {import('next').NextConfig} */
+const WindiCSS = require("windicss-webpack-plugin");
 
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   webpack(config) {
-    config.plugins.push(new WindiCSSWebpackPlugin());
+    config.plugins.push(new WindiCSS());
     return config;
+  },
+
+  images: {
+    domains: ["images.unsplash.com", "deelay.me", "picsum.photos"],
   },
 };
