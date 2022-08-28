9c1487291b26018cb8e2c16d2300efc29738dc74
yk0613 <you@example.com>
2022-08-28 19:29:51 +0900

    Imagecardにダウンロードボタンの追加

--- components/useOnClickOutside.ts
+++ components/useOnClickOutside.ts
@@ -1,28 +1,24 @@
 import { MutableRefObject, useEffect } from "react";
 
 export default function useOnClickOutside(
-  ref: MutableRefObject<HTMLDivElement>,
-  handler: (event: any) => void　
+  ref: MutableRefObject<HTMLDivElement | null>,
+  handler: (event: any) => void
 ) {
-  useEffect(
-    () => {
-      const listener = (event: MouseEvent | TouchEvent) => {
-        // Do nothing if clicking ref's element or descendent elements
-        if (!ref.current || ref.current.contains(event.target as Node)) {
-          return;
-        }
-        handler(event);
-      };
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      // Do nothing if clicking ref's element or descendent elements
+      if (!ref.current || ref.current.contains(event.target as Node)) {
+        return;
+      }
+      handler(event);
+    };
 
-      document.addEventListener("mousedown", listener);
-      document.addEventListener("mousedown", (e) => {});
-      document.addEventListener("touchstart", listener);
-      return () => {
-        document.removeEventListener("mousedown", listener);
-        document.removeEventListener("touchstart", listener);
-      };
-    },
-
-    [ref, handler]
-  );
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("mousedown", (e) => {});
+    document.addEventListener("touchstart", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
 }
