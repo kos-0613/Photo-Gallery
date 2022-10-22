02aa06c50b1bc9a4ed2f0710301b8540ee5a9b06
yk0613 <you@example.com>
2022-10-22 23:04:59 +0900

    テスト環境再構築

---
+++ __test__/Navbar.test.tsx
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Navbar from "../components/Navbar";
+
+it("コスゲリオンが存在するか", () => {
+  render(<Navbar />);
+  expect(screen.getByText("Kosugelion")).toBeInTheDocument();
+});
